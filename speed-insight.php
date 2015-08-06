4dde7c7f61000c120daf62211886919177825776
itlboy <itlboy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-06 17:03:10 +0000

    update description
    git-svn-id: https://plugins.svn.wordpress.org/wp-google-speed-insight/trunk@1214238 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- speed-insight.php
+++ speed-insight.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Performance Check
   Plugin URI: http://blog-itlboy.rhcloud.com
-  Description: With PageSpeed Insights you can identify ways to make your site faster and more mobile-friendly. Page Speed Insights measures the performance of a page for mobile devices and desktop devices. It fetches the url twice, once with a mobile user-agent, and once with a desktop-user agent.
+  Description: With Performance Check you can identify ways to make your site faster and more mobile-friendly. Performance Check measures the performance of a page for mobile devices and desktop devices. It fetches the url twice, once with a mobile user-agent, and once with a desktop-user agent.
   Version: 1.1
   Author: Itlboy
   Author URI: http://blog-itlboy.rhcloud.com
